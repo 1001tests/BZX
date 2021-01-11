babc40bdcd43c3f58fe8fd060d3f20fa4889afbe
202tests <75416919+202tests@users.noreply.github.com>
2021-01-11 09:22:22 +0200

    clean

---
+++ ci/deploy.sh
@@ -0,0 +1,45 @@
+#!/usr/bin/env bash
+
+export LC_ALL=C.UTF-8
+
+cd ..
+cd build-ci/firo-$BUILD_TARGET/src
+mkdir -p bin
+ls -lah
+
+ARCHIVE_CMD="zip"
+
+if [[ $BUILD_TARGET = "win64" ]]; then
+  ARCHIVE_NAME="win64.zip"
+elif [[ $HOST = "x86_64-w64-mingw32" ]]; then
+    ARCHIVE_NAME="windows-x64.zip"
+elif [[ $HOST = "arm-linux-gnueabihf" ]]; then
+    ARCHIVE_NAME="arm-x86.tar.gz"
+    ARCHIVE_CMD="tar -czf"
+elif [[ $HOST = "aarch64-linux-gnu" ]]; then
+    ARCHIVE_NAME="arm-x64.tar.gz"
+    ARCHIVE_CMD="tar -czf"
+elif [[ $HOST = "x86_64-unknown-linux-gnu" ]]; then
+    ARCHIVE_NAME="linux-x64.tar.gz"
+    ARCHIVE_CMD="tar -czf"
+elif [[ $HOST = "x86_64-apple-darwin14" ]]; then
+    ARCHIVE_NAME="osx-x64.zip"
+fi
+
+cp qt/firo-qt bin/ || cp qt/firo-qt.exe bin/ || echo "no qt"
+cp firod bin/ || cp firod.exe bin/ || echo "no daemon"
+cp firo-cli bin/ || cp firo-cli.exe bin/ || echo "no cli"
+cp firo-tx bin/ || cp firo-tx.exe bin/ || echo "no tx"
+echo "before bin"
+
+cd bin || echo "bin failed"
+ls -lah
+strip /* || echo "nothing to strip"
+
+ARCHIVE_CMD="$ARCHIVE_CMD $ARCHIVE_NAME *"
+eval $ARCHIVE_CMD
+
+mkdir -p zip
+mv $ARCHIVE_NAME zip
+
+sleep $(( ( RANDOM % 6 ) + 1 ))s
--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -37,15 +37,12 @@ const std::string CLIENT_NAME("Satoshi");
  * finally CLIENT_VERSION_SUFFIX is added
  */
 
-//! First, include build.h if requested
-#ifdef HAVE_BUILD_INFO
-#include "build.h"
-#endif
 
-//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. $Format:%n#define GIT_ARCHIVE 1$
+//! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
+#define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "$Format:%h$"
-#define GIT_COMMIT_DATE "$Format:%cD$"
+#define GIT_COMMIT_ID "2508162a1"
+#define GIT_COMMIT_DATE "Mon, 30 Nov 2020 10:55:35 +0400"
 #endif
 
 #define BUILD_DESC_WITH_SUFFIX(maj, min, rev, build, suffix) \
