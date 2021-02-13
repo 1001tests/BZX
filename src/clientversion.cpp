7f00352b853b372f850091f93c5f7970d29f5098
202tests <75416919+202tests@users.noreply.github.com>
2021-02-13 04:30:40 +0200

    clean

--- src/clientversion.cpp
+++ src/clientversion.cpp
@@ -41,7 +41,7 @@ const std::string CLIENT_NAME("Satoshi");
 //! git will put "#define GIT_ARCHIVE 1" on the next line inside archives. 
 #define GIT_ARCHIVE 1
 #ifdef GIT_ARCHIVE
-#define GIT_COMMIT_ID "2508162a1"
+#define GIT_COMMIT_ID "travis"
 #define GIT_COMMIT_DATE "Mon, 30 Nov 2020 10:55:35 +0400"
 #endif
 
