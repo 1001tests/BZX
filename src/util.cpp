4f65d3a66ee35f04e0828e62da71ee0060145213
202tests <75416919+202tests@users.noreply.github.com>
2021-02-13 07:59:13 +0200

    Revert "xxxx"
    This reverts commit 2d57e6931254cc7e5091f7bc352b771f04ce7870.

--- src/util.cpp
+++ src/util.cpp
@@ -505,7 +505,7 @@ boost::filesystem::path GetDefaultDataDir()
     // Unix: ~/.BZX
 #ifdef WIN32
     // Windows
-    return GetSpecialFolderPath(CSIDL_APPDATA) / "zzz";
+    return GetSpecialFolderPath(CSIDL_APPDATA) / "firo";
 #else
     fs::path pathRet;
     char* pszHome = getenv("HOME");
@@ -515,10 +515,10 @@ boost::filesystem::path GetDefaultDataDir()
         pathRet = fs::path(pszHome);
 #ifdef MAC_OSX
     // Mac
-    return pathRet / "Library/Application Support/zzz;
+    return pathRet / "Library/Application Support/firo;
 #else
     // Unix
-    return pathRet / ".zzz";;
+    return pathRet / ".firo";;
 #endif
 #endif
 }
