9b8ce74e4cc92f23edc7e43758a59b3f010ca889
202tests <75416919+202tests@users.noreply.github.com>
2021-04-04 19:24:22 +0300

    Update deploy.sh

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -26,7 +26,7 @@ elif [[ $HOST = "x86_64-apple-darwin14" ]]; then
     ARCHIVE_NAME="osx-x64.zip"
 fi
 
-cp qt/firo-qt bin/ || cp qt/bitcoinzero-qt.exe bin/ || echo "no qt"
+cp qt/zcoin-qt bin/ || cp qt/zcoin-qt.exe bin/ || echo "no qt"
 cp firod bin/ || cp firod.exe bin/ || echo "no daemon"
 cp bitcoinzero-cli bin/ || cp bitcoinzero-cli.exe bin/ || echo "no cli"
 cp bitcoinzero-tx bin/ || cp bitcoinzero-tx.exe bin/ || echo "no tx"
