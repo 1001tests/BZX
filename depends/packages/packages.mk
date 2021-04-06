a733e83731c9a51f8a0e04021f65c317f6ff5d92
202tests <75416919+202tests@users.noreply.github.com>
2021-04-06 15:48:42 +0300

    qt

--- depends/packages/packages.mk
+++ depends/packages/packages.mk
@@ -7,7 +7,7 @@ qt_x86_64_linux_packages:= qt expat dbus libxcb xcb_proto libXau xproto freetype
 qt_i686_linux_packages:=$(qt_x86_64_linux_packages)
 
 qt_darwin_packages= qt
-qt_mingw32_packages= 
+qt_mingw32_packages= qt
 
 wallet_packages=bdb
 
@@ -16,5 +16,5 @@ upnp_packages=miniupnpc
 darwin_native_packages = native_biplist native_ds_store native_mac_alias
 
 ifneq ($(build_os),darwin)
-darwin_native_packages += native_cctools native_cdrkit native_libdmg-hfsplus
+darwin_native_packages += native_cctools native_cdrkit natiqtve_libdmg-hfsplus
 endif
