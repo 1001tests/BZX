7c5f3163c6fb3761e70e35e376b53b97ae6a26ba
202tests <75416919+202tests@users.noreply.github.com>
2021-04-10 06:40:29 +0300

    Update params.h

--- src/consensus/params.h
+++ src/consensus/params.h
@@ -266,7 +266,7 @@ struct Params {
     /** Time between blocks for LLMQ random time purposes. Can be less than actual average distance between blocks */
     int nLLMQPowTargetSpacing;
 	
-    int64_t DifficultyAdjustmentInterval() const { 1 };
+    int64_t DifficultyAdjustmentInterval() const { return nPowTargetTimespan / nPowTargetSpacing; }
     uint256 nMinimumChainWork;
     uint256 defaultAssumeValid;
 
